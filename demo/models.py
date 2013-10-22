772500839309bcd5faff4e32c9f1cb5141d0efe2
dpwrussell <douglas.russell@bioch.ox.ac.uk>
2013-10-22 19:00:29 +0100

    Added potential solution using method on Car model

--- demo/models.py
+++ demo/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 # Cars
 class Car(models.Model):
-	name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32)
+
+    def parts_with_manufacturers(self):
+        return self.part_set.select_related('manufacturer')
 
 # Manufacturers
 class Manufacturer(models.Model):
-	name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32)
 
 # Parts
 class Part(models.Model):
-	name = models.CharField(max_length=32)
-	car = models.ForeignKey(Car)
-	manufacturer = models.ForeignKey(Manufacturer)
+    name = models.CharField(max_length=32)
+    car = models.ForeignKey(Car)
+    manufacturer = models.ForeignKey(Manufacturer)
